2952a551a14c0b97db72eae8be9251ff1d141450
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-18 10:51:32 -0500

    add login and current private route

--- api/users.js
+++ api/users.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Models
 const models = require("../models");
@@ -31,13 +32,65 @@ router.post("/register", (req, res) => {
         bcrypt.hash(newUser.password, salt, (error, hash) => {
           // Change password to hash
           newUser.password = hash;
-          newUser.save().then((createdUser) => {
-            return res.json(createdUser);
-          }).catch(err => console.log(err))
+          newUser
+            .save()
+            .then((createdUser) => {
+              return res.json(createdUser);
+            })
+            .catch((err) => console.log(err));
         });
       });
     }
   });
 });
+
+// POST route for users/login (Public)
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  // Find a user via email
+  models.User.findOne({ email }).then((user) => {
+    console.log(`USER: ${user}`);
+    // If there is not a user
+    if (!user) {
+      res.status(400).json({ msg: "User not found" });
+    } else {
+      // If there is a user in the database
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        console.log(`ISMATCH: ${isMatch}`);
+        // Check password for match
+        if (isMatch) {
+          // If user match, send a JSON Web Token
+          // Create a token payload
+          const payload = {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+          };
+          // Sign token
+          jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" }, (error, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`,
+            });
+          });
+        } else {
+          return res
+            .status(400)
+            .json({ msg: "Email or password is incorrect" });
+        }
+      });
+    }
+  });
+});
+
+// GET api/users/current (Private)
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
+})
 
 module.exports = router;
